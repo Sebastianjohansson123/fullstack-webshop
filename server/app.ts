8e87388ac57019c209421820b64f1237df6b2f8d
Thomas ingvarsson <105804552+GunnarIng@users.noreply.github.com>
2023-05-18 14:00:18 +0200

    Can connect to mongoDB and added errorhandling, not validation errors (wip)
    Co-authored-by: Sebastianjohansson123 <Sebastianjohansson123@users.noreply.github.com>

---
+++ server/app.ts
@@ -0,0 +1,36 @@
+import express, { NextFunction, Request, Response } from 'express'; 
+import cookieSession from 'cookie-session';
+
+export const app = express();
+
+app.use(express.json())
+
+
+app.use(cookieSession({
+  name: 'login',
+  secure: false,
+  httpOnly: true,
+  secret: 'ashdasdhj123126tqsa',
+  maxAge: 1000 * 60 * 20,
+}))
+
+
+// Global error handling
+
+
+// Error Handling --------------------------------------------------------------------------------------------------------------------
+app.use((req, res, next) => {
+    res.status(401).json('The resource you are looking for does not exist');
+  });
+  
+  app.use((err: unknown, req: Request, res: Response, next: NextFunction) => {
+    console.error(err);
+    // if (err instanceof ValidationError) {
+    //   res.status(400).json(err.message); }
+     if (err instanceof Error) {
+      res.status(500).json(err.message);
+    } else {
+      res.status(500).json('Unknown server error');
+    }
+  });
+  
