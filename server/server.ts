28f79c5c8545f9e79b7a035966a05bd4a9873031
Thomas ingvarsson <105804552+GunnarIng@users.noreply.github.com>
2023-05-18 14:00:47 +0200

    added mongoose..

---
+++ server/server.ts
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+import { app } from './app'
+
+
+
+async function main() {
+  mongoose.set('strictQuery', false) // Detta är för att få bort varningar i terminalen.app
+  await mongoose.connect('mongodb://127.0.0.1:27017/GentsHatFullStack')
+  console.log("Connected to database")
+
+  app.listen(3000, () => {
+    console.log("Server running on port 3000")
+  })
+}
+
+main().catch(console.error)
