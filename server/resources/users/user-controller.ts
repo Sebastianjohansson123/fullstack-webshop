0c030ea63daeb7a46467aab5b8acc60e9be77b26
Sebastianjohansson123 <johansson.sebastian92@hotmail.com>
2023-05-19 11:02:01 +0200

    added all the functions needed in the user-controller ( Not the logic yet )
    wip
    
    Co-authored-by: Thomas ingvarsson <thomas.g.ingvarsson@gmail.com>
    Co-authored-by: Mergim Shala <mergimshalaa@users.noreply.github.com>

---
+++ server/resources/users/user-controller.ts
@@ -0,0 +1,3 @@
+export async function registerUser(req: Request, res: Response) {}
+export async function loginUser(req: Request, res: Response) {}
+export async function logoutUser(req: Request, res: Response) {}
