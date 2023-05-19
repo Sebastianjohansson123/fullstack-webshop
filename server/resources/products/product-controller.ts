4e58bdcaf115080458df2b51da187e66b746c774
Sebastianjohansson123 <johansson.sebastian92@hotmail.com>
2023-05-19 11:01:11 +0200

    added all the functions needed in the product-controller ( Not the logic yet )
    wip
    
    Co-authored-by: Thomas ingvarsson <thomas.g.ingvarsson@gmail.com>
    Co-authored-by: Mergim Shala <mergimshalaa@users.noreply.github.com>

---
+++ server/resources/products/product-controller.ts
@@ -0,0 +1,7 @@
+// importera ProductModel
+
+export async function getAllProducts(req: Request, res: Response) {}
+export async function getProductById(req: Request, res: Response) {}
+export async function addProduct(req: Request, res: Response) {}
+export async function updateProduct(req: Request, res: Response) {}
+export async function deleteProduct(req: Request, res: Response) {}
