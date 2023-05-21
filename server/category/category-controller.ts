c5df07afb1793be4b6b888670fa1b85a4d440f87
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-05-21 11:23:47 +0200

    add category retrieval functions with console log

---
+++ server/category/category-controller.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+
+export async function getAllCategories(
+    req: Request,
+    res: Response
+  ) {
+      return console.log('Get all Categories')
+  }
+
+  export async function getCategoriesById(
+    req: Request,
+    res: Response
+  ) {
+      return console.log('Retrieving category by ID')
+  }
