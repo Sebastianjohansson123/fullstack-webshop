4fb5f38ba67b6f434e3d0b4a4b7b8aa594b170ad
Sebastianjohansson123 <johansson.sebastian92@hotmail.com>
2023-05-22 15:40:19 +0200

    renamed the DB name ( GentsShop )
    Co-authored-by: Yehad <ye-mou@users.noreply.github.com>
    Co-authored-by: Thomas ingvarsson <thomas.g.ingvarsson@gmail.com>
    Co-authored-by: Mergim Shala <mergimshalaa@users.noreply.github.com>

--- server/server.ts
+++ server/server.ts
@@ -1,16 +1,14 @@
-import mongoose from 'mongoose'
-import { app } from './app'
-
-
+import mongoose from 'mongoose';
+import { app } from './app';
 
 async function main() {
-  mongoose.set('strictQuery', false) // Detta är för att få bort varningar i terminalen.app
-  await mongoose.connect('mongodb://127.0.0.1:27017/GentsHatFullStack')
-  console.log("Connected to database")
+  mongoose.set('strictQuery', false); // Detta är för att få bort varningar i terminalen.app
+  await mongoose.connect('mongodb://127.0.0.1:27017/GentsShop');
+  console.log('Connected to database');
 
   app.listen(3000, () => {
-    console.log("Server running on port 3000")
-  })
+    console.log('Server running on port 3000');
+  });
 }
 
-main().catch(console.error)
+main();
