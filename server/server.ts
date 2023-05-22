1203dd7e8de953f059dd4d95d3d8f1cf7784e8ed
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-05-22 16:26:56 +0200

    connected to db compass with key

--- server/server.ts
+++ server/server.ts
@@ -3,7 +3,7 @@ import { app } from './app';
 
 async function main() {
   mongoose.set('strictQuery', false); // Detta är för att få bort varningar i terminalen.app
-  await mongoose.connect('mongodb://127.0.0.1:27017/GentsShop');
+  await mongoose.connect('mongodb+srv://admin:1IsBetPbWFXS98uJ@gentshatwebshop.c0bm92t.mongodb.net/');
   console.log('Connected to database');
 
   app.listen(3000, () => {
