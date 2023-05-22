ab890d0659500e789bf4f6991c5e42c0b8d72921
Sebastianjohansson123 <johansson.sebastian92@hotmail.com>
2023-05-22 15:43:12 +0200

    fixed the order router & controller according to the ERD
    Co-authored-by: Yehad <ye-mou@users.noreply.github.com>
    Co-authored-by: Thomas ingvarsson <thomas.g.ingvarsson@gmail.com>
    Co-authored-by: Mergim Shala <mergimshalaa@users.noreply.github.com>

---
+++ server/resources/orders/order-controller.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from 'express';
+
+export async function getOrders(req: Request, res: Response) {}
+export async function getOrdersByUserId(req: Request, res: Response) {}
+export async function getOrderById(req: Request, res: Response) {}
+export async function updateOrderById(req: Request, res: Response) {}
+export async function createOrder(req: Request, res: Response) {}
+export async function getProductsByCategory(req: Request, res: Response) {}
+export async function addProduct(req: Request, res: Response) {}
--- server/resources/orders/order-router.ts
+++ server/resources/orders/order-router.ts
@@ -1,3 +1,18 @@
 import express from 'express';
+import { adminAuth } from '../../middlewares/adminAuth';
+import { auth } from '../../middlewares/userAuth';
+import {
+  createOrder,
+  getOrderById,
+  getOrders,
+  getOrdersByUserId,
+  updateOrderById,
+} from './order-controller';
 
-export const orderRouter = express.Router();
+export const orderRouter = express
+  .Router()
+  .get('api/orders', adminAuth, getOrders)
+  .get('api/orders/user/:userid', auth, getOrdersByUserId)
+  .get('api/orders/:id', adminAuth, getOrderById)
+  .put('api/orders/:id', adminAuth, updateOrderById)
+  .post('api/orders', auth, createOrder);
