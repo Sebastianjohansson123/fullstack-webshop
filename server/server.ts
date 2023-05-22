925d983f5fde9d52c82995d2a5f6baaebc92aa69
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-05-22 16:28:36 +0200

    comment to connect with db

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 import { app } from './app';
 
+//För att koppla er ti db compass kopiera detta mongodb+srv://admin:1IsBetPbWFXS98uJ@gentshatwebshop.c0bm92t.mongodb.net/
+
 async function main() {
   mongoose.set('strictQuery', false); // Detta är för att få bort varningar i terminalen.app
   await mongoose.connect('mongodb+srv://admin:1IsBetPbWFXS98uJ@gentshatwebshop.c0bm92t.mongodb.net/');
