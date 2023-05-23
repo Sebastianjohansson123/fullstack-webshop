4538334138595e940140c702ba45df748b7bad79
Sebastianjohansson123 <johansson.sebastian92@hotmail.com>
2023-05-23 12:23:37 +0200

    added uploadImage code to the function.
    wip
    
    Co-authored-by: Thomas ingvarsson <thomas.g.ingvarsson@gmail.com>

--- server/resources/images/image-controller.ts
+++ server/resources/images/image-controller.ts
@@ -1,8 +1,27 @@
+import busboy from 'busboy';
 import { Request, Response } from 'express';
+import mongoose from 'mongoose';
+import sharp from 'sharp';
+import { fileBucket } from './image-model';
 
 export async function getImageById(req: Request, res: Response) {
   // logic to get an image by id
 }
 export async function uploadImage(req: Request, res: Response) {
-  // logic for upload
+  const bb = busboy({ headers: req.headers });
+  req.pipe(bb);
+
+  bb.on('file', (_, file, info) => {
+    const { filename, mimeType } = info;
+
+    const uploadStream = fileBucket
+      .openUploadStream(filename, { contentType: mimeType })
+      .on('finish', (data: mongoose.mongo.GridFSFile) => {
+        res.status(201).json(data._id);
+      });
+
+    const resizer = sharp().resize(300).jpeg({ quality: 90 });
+
+    file.pipe(resizer).pipe(uploadStream);
+  });
 }
