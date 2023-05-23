c367e4ac47ebbee202ba9e6511d9f22a8597d019
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-05-23 13:35:05 +0200

    wip
    Co-authored-by: Yehad <ye-mou@users.noreply.github.com>

--- server/resources/users/user-controller.ts
+++ server/resources/users/user-controller.ts
@@ -1,6 +1,51 @@
 import { Request, Response } from 'express';
+import { UserModel } from './user-model';
+import * as yup from "yup";
 
-export async function registerUser(req: Request, res: Response) {}
+export async function registerUser(req: Request, res: Response) {
+  const {username, password} = req.body;
+
+  // Checks for missing or incorrect values
+  const userSchema = yup.object().shape({
+    username: yup.string().strict().required(),
+    password: yup.string().strict().required(),
+  });
+
+  const result = userSchema.validate(req.body);
+
+  if (result instanceof yup.ValidationError) {
+    res.status(400).json(result.errors);
+    return;
+  }
+
+
+  //Checking username to existing one
+  const existingUser = await UserModel.findOne({
+    username,
+  });
+  if (existingUser) {
+    res
+      .status(409)
+      .json(
+        "This username is taken. Please chose another one"
+      );
+    return;
+  }
+
+  const user = {
+    username,
+    password,
+  };
+  const newUser = await UserModel.create(user);
+
+  res.status(201).json({
+    _id: newUser._id,
+    username: newUser.username,
+    isAdmin: newUser.isAdmin,
+  });
+
+}
+
 export async function loginUser(req: Request, res: Response) {}
 export async function logoutUser(req: Request, res: Response) {}
 export async function getUserById(req: Request, res: Response) {}
