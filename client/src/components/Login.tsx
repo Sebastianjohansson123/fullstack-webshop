83f7ed6d2b71e760ce8a5c1bb0d202d3193c6526
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-05-23 11:21:25 +0200

    login component & add pic for loginform
    Co-authored-by: Yehad <ye-mou@users.noreply.github.com>

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,8 +1,34 @@
+import { Box, Button, Container, TextField } from "@mui/material";
+import { Link, NavLink } from "react-router-dom";
+import ManInHat from '../icons/manInHat.png'
+
 function LoginPage() {
-    return (
-        <>
-        </>
+    return(
+        <Container style={{ display: 'flex', justifyContent: 'center'}}>
+            <Box
+                display={"flex"}
+                alignItems={'center'}
+                flexDirection={'column'}
+                gap={'10px'}
+                padding={'9rem 0'}
+                sx={{ width: "100%", maxWidth: "400px" }}
+                >
+                    <img src={ManInHat}/>
+            <span style={{ fontSize: '30px'}}>Log in</span>
+            <TextField fullWidth id="outlined-basic" label="Email" type="email" variant="outlined" />
+            <TextField fullWidth id="outlined-basic" label="Password" type="password" variant="outlined" />
+            <Button
+                    component={Link}
+                    to="/"
+                    variant="contained"
+                    sx={{ boxShadow: "none", marginTop: "1rem" }}
+                >
+                    Log in
+                </Button>
+                <NavLink to="/register">Don't have an account? Sign up</NavLink>
+            </Box>
+        </Container>
     )
-};
+}
 
 export default LoginPage;
