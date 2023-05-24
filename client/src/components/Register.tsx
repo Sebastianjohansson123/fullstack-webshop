e8e0130678be3e4f37e237d17a3a0e890c1d2b88
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-05-24 21:49:10 +0200

    formikvalidation for register an user

--- client/src/components/Register.tsx
+++ client/src/components/Register.tsx
@@ -1,31 +1,55 @@
 import { Box, Button, TextField } from '@mui/material';
-import ManInHat from '../icons/manInHat.png';
+import { useFormik } from 'formik';
 import { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import * as Yup from 'yup';
+import ManInHat from '../icons/manInHat.png';
+
+
+const registerSchema = Yup.object({
+  username: Yup.string().required("Please enter an username!"),
+  password: Yup.string().required("Please enter your password!")
+})
+type RegisterValues = Yup.InferType<typeof registerSchema>
 
 function RegisterPage() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-  const handleRegisterAccount = async (e: React.FormEvent) => {
-    e.preventDefault();
-
+  const handleRegisterAccount = async (username: string, password: string) => {
     const newUser = {
       username,
       password
     };
-  
+
     const response = await fetch("/api/users/register", {
       method: "POST",
       body: JSON.stringify(newUser),
       headers: { "Content-type": "application/json" },
     });
-  
+
       const data = await response.json();
     if (response.ok) {
       localStorage.setItem("loggedInUsername", data.username);
       localStorage.setItem("loggedInUserID", data._id);
     }
   };
+
+
+    const formik = useFormik<RegisterValues>({
+      initialValues: {
+        username: "",
+        password: "",
+      },
+      validationSchema: registerSchema,
+      onSubmit: async (registerValues) => {
+        const loggedinUser = await handleRegisterAccount(registerValues.username, registerValues.password);
+        console.log(loggedinUser);
+        navigate("/");
+      },
+    });
+
   return (
     <>
       <Box
@@ -38,15 +62,18 @@ function RegisterPage() {
           height: '75vh',
         }}
       >
-        <img src={ManInHat} />
+       <img src={ManInHat} />
         <Box sx={{ width: '100%', maxWidth: '400px' }}>
-          <form onSubmit={handleRegisterAccount}> 
+          <form onSubmit={formik.handleSubmit}> 
             <TextField
               id='username'
               label='Username'
               name='username'
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
+              value={formik.values.username}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={Boolean(formik.touched.username && formik.errors.username)}
+              helperText={formik.touched.username && formik.errors.username}
               type='text'
               fullWidth
               required
@@ -57,8 +84,11 @@ function RegisterPage() {
               id='password'
               label='Password'
               type='password'
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={Boolean(formik.touched.password && formik.errors.password)}
+              helperText={formik.touched.password && formik.errors.password}
               fullWidth
               required
               margin='normal'
