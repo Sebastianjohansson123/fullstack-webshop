9e828869cbc32715586d4d105fc0c1d7a8dd0654
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-05-25 14:50:09 +0200

    wip for private router component
    Co-authored-by: Sebastianjohansson123 <Sebastianjohansson123@users.noreply.github.com>
    Co-authored-by: Thomas ingvarsson <thomas.g.ingvarsson@gmail.com>
    Co-authored-by: Yehad <ye-mou@users.noreply.github.com>

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,17 +4,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {
   createBrowserRouter,
-  createRoutesFromElements,
+  Navigate,
   Route,
+  RouteProps,
   RouterProvider,
+  useLocation,
+  useRoutes,
+  createRoutesFromElements
 } from 'react-router-dom';
 import App from './App';
 import Login from './components/Login';
 import Register from './components/Register';
 import { CartProvider } from './contexts/CartContext';
-import { UserProvider } from './contexts/UserContext';
 import { FormProvider } from './contexts/FormContext';
 import { ProductsProvider } from './contexts/ProductsContext';
+import { UserProvider, useUserContext } from './contexts/UserContext';
 import './index.css';
 import Admin from './pages/Admin';
 import AdminUpdateDatabase from './pages/AdminUpdateDatabase';
@@ -22,6 +26,7 @@ import Checkout from './pages/Checkout';
 import Home from './pages/Home';
 import OrderConfirmation from './pages/OrderConfirmation';
 import ProductDescription from './pages/ProductDescription';
+
 
 declare module '@mui/material/styles' {
   interface ThemeOptions {
@@ -110,13 +115,49 @@ const router = createBrowserRouter(
       <Route path='login' element={<Login />} />
       <Route path='/register' element={<Register />} />
       <Route path='checkout' element={<Checkout />} />
-      <Route path='admin' element={<Admin />} />
-      <Route path='admin/product/:id' element={<AdminUpdateDatabase />} />
+      {/* <PrivateRoute path='/user' element={<UserPage />} /> */}
+      <PrivateRoute  path='admin' element={<Admin />} adminOnly />
+      <PrivateRoute  path='admin/product/:id' element={<AdminUpdateDatabase />} adminOnly />
       <Route path='confirmation' element={<OrderConfirmation />} />
       <Route path='*' element={<h2>404 not found</h2>} />
     </Route>
   )
 );
+
+// function RouteComponents() {
+//   const routes = useRoutes([
+//    { path: '/', element: <App /> },
+//    { path: '/', element: <Home />, index: true },
+//    { path: '/:page/:id', element: <ProductDescription /> },
+//    { path: 'login', element: <Login /> },
+//    { path: 'register', element: <Register /> },
+//    { path: 'checkout', element: <Checkout /> },
+//    { path: 'admin', element: <PrivateRoute adminOnly><Admin /></PrivateRoute> },
+//    { path: 'admin/product/:id', element: <PrivateRoute adminOnly><AdminUpdateDatabase /></PrivateRoute> },
+//    { path: 'confirmation', element: <OrderConfirmation /> },
+//    { path: '*', element: <h2>404 not found</h2> },
+//   ]);
+//   return routes
+// }
+
+
+
+
+function PrivateRoute(props: RouteProps & { adminOnly?: boolean }) {
+  const { user, isLoading } = useUserContext();  //   DESSA RADERNA!!!
+  const location = useLocation();               //    DESSA RADERNA!!!
+
+  // I Login komponenten
+  // location.state?.redirectTo || user.isAdmin ? "/admin" : "/user"
+  
+  if (isLoading) return null;
+  if (props.adminOnly && !user?.isAdmin) {
+    return <Navigate to='/' state={{ redirectTo: location }} />;
+  }
+  if (!user) return <Navigate to='/login' state={{ redirectTo: location }} />;
+  return <Route {...props} />;
+}
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
