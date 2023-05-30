268a82365c106b3d132b2d4ce8d71ce11c9731ea
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-05-30 10:31:18 +0200

    endpoints category to get allcategories and byName

--- server/resources/categories/category-controller.ts
+++ server/resources/categories/category-controller.ts
@@ -1,9 +1,16 @@
 import { Request, Response } from 'express';
+import { CategoryModel } from './category-model';
 
 export async function getAllCategories(req: Request, res: Response) {
-  console.log('Get all Categories');
-}
+  const categories = await CategoryModel.find();
+  res.json(categories);
+};
 
 export async function getCategoriesByName(req: Request, res: Response) {
-  console.log('Retrieving category by name');
-}
+  const category = await CategoryModel.findById(req.params.id);
+  if (category) {
+    res.json(category);
+  } else {
+    return res.status(404).json({ error: 'Category not found!' });
+  }
+};
