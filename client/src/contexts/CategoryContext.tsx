7d27bdc05c88c8abfc5703948a64de528c1f23a4
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-05-31 10:47:33 +0200

    outcomment categoryContext for now

--- client/src/contexts/CategoryContext.tsx
+++ client/src/contexts/CategoryContext.tsx
@@ -1,70 +1,64 @@
-import { Dispatch, PropsWithChildren, SetStateAction, createContext, useContext, useEffect, useState } from 'react';
-import { Product } from '../../data';
+// import { Dispatch, PropsWithChildren, SetStateAction, createContext, useContext, useEffect, useState } from 'react';
+// import { Product } from '../../data';
 
+// interface CategoryContextValues {
+//     categories: string[];
+//     choosenCategory: string;
+//     productsByChoosenCategory: Product[];
+//     setChoosenCategory: Dispatch<SetStateAction<string>>;
+//     setProductsByChoosenCategory: Dispatch<SetStateAction<Product[]>>;
+//   }
 
+// const CategoryContext = createContext<CategoryContextValues>(
+//   null as any
+// );
 
-interface CategoryContextValues {
-    categories: string[];
-    choosenCategory: string;
-    productsByChoosenCategory: Product[];
-    setChoosenCategory: Dispatch<SetStateAction<string>>;
-    setProductsByChoosenCategory: Dispatch<SetStateAction<Product[]>>;
-  }
+// export function useCategory() {
+//   return useContext(CategoryContext);
+// }
 
-const CategoryContext = createContext<CategoryContextValues>(
-  null as any
-);
+// export const CategoryProvider = (props: PropsWithChildren) => {
+//   const [categories, setCategories] = useState([]);
+//   const [choosenCategory, setChoosenCategory] = useState<string>('all');
+//   const [productsByChoosenCategory, setProductsByChoosenCategory] = useState<Product[]>([]);
 
-export function useCategory() {
-  return useContext(CategoryContext);
-}
+//   useEffect(() => {
+//     const fetchCategories = async () => {
+//       try {
+//         const response = await fetch('/api/categories');
+//         const data = await response.json();
+//         setCategories(data);
+//       } catch (error) {
+//         console.log(error);
+//       }
+//     };
 
-export const CategoryProvider = (props: PropsWithChildren) => {
-  const [categories, setCategories] = useState([]);
-  const [choosenCategory, setChoosenCategory] = useState<string>('all');
-  const [productsByChoosenCategory, setProductsByChoosenCategory] = useState<Product[]>([]);
+//     fetchCategories();
+//   }, []);
 
-  useEffect(() => {
-    const fetchCategories = async () => {
-      try {
-        const response = await fetch('/api/categories');
-        const data = await response.json();
-        setCategories(data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
+//   // Context provider
 
-    fetchCategories();
-  }, []);
+//   useEffect(() => {
+//     const fetchProductsByCategory = async () => {
+//       try {
+//         const response = await fetch(
+//           `/api/products/category/${choosenCategory}`
+//         );
+//         const data = await response.json();
+//         setProductsByChoosenCategory(data);
+//       } catch (error) {
+//         console.log(error);
+//       }
+//     };
+//     fetchProductsByCategory();
+//   }, [choosenCategory]);
 
-
-
-
-  // Context provider
-
-
-  useEffect(() => {
-    const fetchProductsByCategory = async () => {
-      try {
-        const response = await fetch(
-          `/api/products/category/${choosenCategory}`
-        );
-        const data = await response.json();
-        setProductsByChoosenCategory(data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchProductsByCategory();
-  }, [choosenCategory]);
-
-  // Variables and functions that the context shares
-  return (
-    <CategoryContext.Provider
-      value={{ categories, choosenCategory, productsByChoosenCategory, setChoosenCategory, setProductsByChoosenCategory }}
-    >
-      {props.children}
-    </CategoryContext.Provider>
-  );
-};
+//   // Variables and functions that the context shares
+//   return (
+//     <CategoryContext.Provider
+//       value={{ categories, choosenCategory, productsByChoosenCategory, setChoosenCategory, setProductsByChoosenCategory }}
+//     >
+//       {props.children}
+//     </CategoryContext.Provider>
+//   );
+// };
