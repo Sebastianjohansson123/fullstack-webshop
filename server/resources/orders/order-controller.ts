0e334ed9e2c2e65bd89d33bdc8f647497ff3dd5d
Sebastianjohansson123 <johansson.sebastian92@hotmail.com>
2023-06-01 22:34:08 +0200

    npm run format

--- server/resources/orders/order-controller.ts
+++ server/resources/orders/order-controller.ts
@@ -3,7 +3,6 @@ import { array, number, object } from 'yup';
 import { ProductModel } from '../products/product-model';
 import { OrderModel } from './order-model';
 
-
 export async function getOrders(req: Request, res: Response) {
   const orders = await OrderModel.find();
   res.status(200).json(orders);
