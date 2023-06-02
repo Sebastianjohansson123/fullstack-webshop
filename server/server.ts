a478f1fa609363e3fbb5221c16f84bc5613187c5
Sebastianjohansson123 <johansson.sebastian92@hotmail.com>
2023-06-02 14:27:30 +0200

    added dot env file

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,17 @@
+import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import { app } from './app';
 
 //För att koppla er ti db compass kopiera detta mongodb+srv://admin:1IsBetPbWFXS98uJ@gentshatwebshop.c0bm92t.mongodb.net/
 
+dotenv.config();
+
 async function main() {
+  if (!process.env.MONGO_URI) {
+    throw new Error('Missing MONGO_URI env variable');
+  }
   mongoose.set('strictQuery', false); // Detta är för att få bort varningar i terminalen.app
-  await mongoose.connect('mongodb+srv://admin:1IsBetPbWFXS98uJ@gentshatwebshop.c0bm92t.mongodb.net/');
+  await mongoose.connect(process.env.MONGO_URI);
   console.log('Connected to database');
 
   app.listen(3000, () => {
